947a69c94264a0581b9e8d1a7940cfe32bdbe923
Ananth Narayan <narayanananth26@gmail.com>
2023-09-25 19:49:29 +0530

    create events page

--- src/components/Navigation/NavLeft.jsx
+++ src/components/Navigation/NavLeft.jsx
@@ -1,5 +1,31 @@
+"use client";
+
+import { leftNavLinks } from "@utils/constants/links";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const NavLeft = () => {
-	return <div>NavLeft</div>;
+	const pathname = usePathname();
+	return (
+		<div className="flex-start gap-10">
+			{leftNavLinks.map((link, index) => {
+				const isActive =
+					(pathname.includes(link.route) && link.route.length > 1) ||
+					pathname === link.route;
+				return (
+					<Link
+						key={index}
+						href={link.route}
+						className={`font-oswald ${
+							isActive ? "bg-red-500 text-white" : "text-black"
+						}`}
+					>
+						{link.label}
+					</Link>
+				);
+			})}
+		</div>
+	);
 };
 
 export default NavLeft;
--- src/utils/constants/links.js
+++ src/utils/constants/links.js
@@ -2,3 +2,14 @@ export const homeLink = "/";
 export const forgotPasswordLink = "/forgot-password";
 export const signInLink = "/sign-in";
 export const registerLink = "/register";
+
+export const leftNavLinks = [
+	{
+		route: "/events",
+		label: "Events",
+	},
+	{
+		route: "/",
+		label: "Athletes",
+	},
+];
