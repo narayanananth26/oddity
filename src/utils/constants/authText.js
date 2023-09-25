ec4692b6cc38cc1f62552c75046076e5026ecbdc
Ananth Narayan <narayanananth26@gmail.com>
2023-09-25 20:34:06 +0530

    create authText.jsx

---
+++ src/utils/constants/authText.js
@@ -0,0 +1,18 @@
+export const authTexts = [
+	{
+		route: "/sign-in",
+		message: "Ready to Score Big?",
+	},
+	{
+		route: "/register",
+		message: "Ready to Score Big?",
+	},
+	{
+		route: "/forgot-password",
+		message: "Ready to Score Big?",
+	},
+	{
+		route: "/reset-password",
+		message: "Ready to Score Big?",
+	},
+];
