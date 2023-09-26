f15f4daf402279fabb5f31d460218840017c7dcc
Ananth Narayan <narayanananth26@gmail.com>
2023-09-26 16:07:42 +0530

    style RootNav comp

--- src/components/Navigation/RootNav.jsx
+++ src/components/Navigation/RootNav.jsx
@@ -1,5 +1,49 @@
+"use client";
+
+import { leftNavLinks, rightNavLinks } from "@utils/constants/links";
+import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const { default: NavLayout } = require("./NavLayout");
+
 const RootNav = () => {
-	return <div>RootNav</div>;
+	const pathname = usePathname();
+
+	return (
+		<NavLayout>
+			<div className="flex-start gap-10">
+				{leftNavLinks.map((link, index) => {
+					const isActive =
+						(pathname.includes(link.route) &&
+							link.route.length > 1) ||
+						pathname === link.route;
+					return (
+						<NavLink key={index} link={link} isActive={isActive} />
+					);
+				})}
+			</div>
+
+			<Link
+				href="/"
+				className="flex-center uppercase text-red-500 text-7xl font-medium"
+			>
+				Oddity
+			</Link>
+
+			<div className="flex-end gap-10">
+				{rightNavLinks.map((link, index) => {
+					const isActive =
+						(pathname.includes(link.route) &&
+							link.route.length > 1) ||
+						pathname === link.route;
+					return (
+						<NavLink key={index} link={link} isActive={isActive} />
+					);
+				})}
+			</div>
+		</NavLayout>
+	);
 };
 
 export default RootNav;
