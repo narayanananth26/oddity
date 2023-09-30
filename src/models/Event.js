65ad0985c05ad6ff2171f23b272b8a859ae34b73
Ananth Narayan <narayanananth26@gmail.com>
2023-09-30 15:27:24 +0530

    Create event schema in models/Event.js

---
+++ src/models/Event.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+
+const eventSchema = new Schema({
+	name: {
+		type: String,
+		required: [true, "Event name is required."],
+		trim: true,
+	},
+	description: {
+		type: String,
+		required: [true, "Event description is required."],
+		trim: true,
+	},
+	date: {
+		type: Date,
+		required: [true, "Event date is required."],
+	},
+	sport: {
+		type: String,
+		required: [true, "Sport type is required."],
+		enum: {
+			values: ["Football", "Cricket", "Other"],
+			message: "Invalid sport type.",
+		},
+	},
+	home_team: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Team",
+		required: [true, "Home team is required."],
+	},
+	away_team: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Team",
+		required: [true, "Away team is required."],
+	},
+	odds: {
+		home_team: {
+			type: Number,
+			required: [true, "Home team odds are required."],
+		},
+		away_team: {
+			type: Number,
+			required: [true, "Away team odds are required."],
+		},
+		draw: {
+			type: Number,
+			required: [true, "Draw odds are required."],
+		},
+	},
+	venue: {
+		name: {
+			type: String,
+			required: [true, "Venue name is required."],
+			trim: true,
+		},
+		location: {
+			type: String,
+			required: [true, "Venue location is required."],
+			trim: true,
+		},
+	},
+	status: {
+		type: String,
+		enum: {
+			values: ["Scheduled", "Live", "Completed", "Postponed", "Canceled"],
+			message: "Invalid event status.",
+		},
+		default: "Scheduled",
+	},
+	number_of_bets_placed: {
+		type: Number,
+		default: 0,
+	},
+	winner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Team",
+		default: null,
+	},
+});
+
+export default mongoose.models.Event || mongoose.model("Event", eventSchema);
