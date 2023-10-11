8dd6ed4797f808b76fcaf289b103b318894bbe19
Ananth Narayan <narayanananth26@gmail.com>
2023-10-11 22:40:24 +0530

    update ImageBanner.jsx

--- src/components/UI/ImageBanner.jsx
+++ src/components/UI/ImageBanner.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { HiChevronRight } from "react-icons/hi2";
 
 const ImageBanner = ({ src, alt }) => {
 	const [isHovered, setIsHovered] = useState(false);
@@ -13,25 +14,35 @@ const ImageBanner = ({ src, alt }) => {
 					alt={alt}
 					fill
 					className={`transition-transform transform-gpu ease-in-out duration-700 object-cover object-center ${
-						isHovered && "scale-125 bg-blend-overlay"
+						isHovered && "scale-125 blur-sm"
 					}`}
 				/>
 			</div>
 
 			{/* Overlay */}
 			<div
-				className={`absolute inset-0 bg-black transition-opacity ease-in-out duration-500 ${
-					isHovered ? "opacity-50" : "opacity-0"
+				className={`absolute inset-0 bg-black transition-opacity ease-in-out duration-500 opacity-70 ${
+					isHovered && ""
 				}`}
 			/>
 
+			<div className="flex flex-col flex-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-2">
+				<h1 className="text-5xl text-white uppercase font-bold">
+					Ind vs Aus
+				</h1>
+				<p className="text-center text-lg text-white font-bold">
+					The clash between India and Australia promises an exciting
+					battle of skills and strategy on the pitch.
+				</p>
+			</div>
+
 			{/* Button */}
 			<button
-				className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 bg-white rounded-lg shadow-lg`}
+				className={`flex-center text-white text-7xl font-bold uppercase absolute bottom-0 left-0 transform translate-x-1/4 -translate-y-1/2 px-6 py-3 hover:text-red-500`}
 				onMouseEnter={() => setIsHovered(true)}
 				onMouseLeave={() => setIsHovered(false)}
 			>
-				Hover to Zoom
+				Wager Now <HiChevronRight />
 			</button>
 		</section>
 	);
