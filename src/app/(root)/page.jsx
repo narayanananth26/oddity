f9c3e7736fcbef622b63c41d0d4257aef0fcf9b6
Ananth Narayan <narayanananth26@gmail.com>
2023-10-15 14:02:20 +0530

    Update /page.jsx
    Implement fetching data for ImageBanner.jsx from API endpoint

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,10 +1,37 @@
+"use client";
 import ImageBanner from "@components/UI/ImageBanner";
 import UnderConstruction from "@components/UI/UnderConstruction";
+import { apiUrl } from "@utils/constants/links";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+	const [events, setEvents] = useState([]);
+
+	useEffect(() => {
+		fetch(`${apiUrl}/events?filterBy=status&status=Live`)
+			.then((response) => response.json())
+			.then((data) => setEvents(data))
+			.catch((error) => console.error("Error fetching events.", error));
+	}, []);
+
+	let event;
+
+	if (events) {
+		event = events.reduce(
+			(maxEvent, event) => {
+				if (
+					event.number_of_bets_placed > maxEvent.number_of_bets_placed
+				) {
+					return event;
+				}
+				return maxEvent;
+			},
+			{ number_of_bets_placed: -1 }
+		);
+	}
 	return (
 		<>
-			<ImageBanner src="/img.webp" alt="banner" />
+			<ImageBanner src="/img.webp" alt="banner" data={event} />
 			<UnderConstruction />
 		</>
 	);
