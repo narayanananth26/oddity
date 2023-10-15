7a8043b879c4deedcd7f8da15ed259750b397b32
Ananth Narayan <narayanananth26@gmail.com>
2023-10-15 14:07:35 +0530

    Update '/api/events/route.js'
    Implement API endpoint to fetch events

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,107 +1,23 @@
 import Event from "@models/Event";
 import { connectToMongoDB } from "@utils/database/connectToMongoDB";
-import { NextResponse } from "next";
+import { NextResponse } from "next/server";
 
-export default handleRequest = async (req) => {
-	if (req.method === "GET") {
-		if (req.query.filterBy === "sport") {
-			return getEventBySport(req);
-		} else if (req.query.filterBy === "status") {
-			return getEventByStatus(req);
-		} else {
-			return new NextResponse("Invalid filterBy parameter", {
-				status: 400,
-			});
-		}
-	} else if (req.method === "POST") {
-		return createEvent(req);
-	} else if (req.method === "PATCH") {
-		return updateEvent(req);
-	} else if (req.method === "DELETE") {
-		return deleteEvent(req);
-	}
-
-	return new NextResponse("Invalid HTTP method", {
-		status: 405,
-	});
-};
-
-async function getEventBySport(req) {
-	try {
-		const { sport } = req.query;
-
-		if (!sport) {
-			return new NextResponse("Sport parameter is required", {
-				status: 400,
-			});
-		}
-
-		await connectToMongoDB();
-		const events = await Event.find({ sport });
-
-		if (!events || events.length === 0) {
-			return new NextResponse("No events found for the specified sport", {
-				status: 404,
-			});
-		}
-
-		return new NextResponse(events, {
-			status: 200,
-		});
-	} catch (error) {
-		return new NextResponse(
-			{ error: "Error fetching events by sport" },
-			{
-				status: 500,
-			}
-		);
-	}
-}
-
-async function getEventByStatus(req) {
-	const { status } = req.query;
-
-	if (!status) {
-		return new NextResponse("Status parameter is required", {
-			status: 400,
-		});
-	}
+export const GET = async (request) => {
+	const filterBy = request.nextUrl.searchParams.get("filterBy");
+	const filter = request.nextUrl.searchParams.get(filterBy);
 
 	await connectToMongoDB();
-
 	try {
-		const events = await Event.find({ status });
+		let query = { [filterBy]: filter };
+		const filteredEvents = await Event.find(query);
 
-		if (!events || events.length === 0) {
-			return new NextResponse(
-				"No events found with the specified status",
-				{
-					status: 404,
-				}
-			);
-		}
-
-		return new NextResponse(events, {
+		return new NextResponse(JSON.stringify(filteredEvents), {
 			status: 200,
 		});
 	} catch (error) {
-		return new NextResponse(
-			{ error: "Error fetching events by status" },
-			{
-				status: 500,
-			}
-		);
+		console.log(error);
+		return new NextResponse(error.message, {
+			status: 500,
+		});
 	}
-}
-
-async function createEvent(req) {
-	// !POST
-}
-
-async function updateEvent(req) {
-	// !PATCH
-}
-
-async function deleteEvent(req) {
-	// !DELETE
-}
+};
