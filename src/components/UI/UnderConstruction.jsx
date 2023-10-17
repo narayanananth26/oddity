ac4e6af86da30d3f7bf70551587ca1581011c216
Ananth Narayan <narayanananth26@gmail.com>
2023-10-17 15:30:45 +0530

    Add link to "/"

--- src/components/UI/UnderConstruction.jsx
+++ src/components/UI/UnderConstruction.jsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
+
 const UnderConstruction = () => {
 	return (
 		<div className="bg-black flex-center flex-col">
 			{Array.from({ length: 100 }, (_, index) => (
-				<div
+				<Link
+					href="/"
 					key={index}
 					className="font-oswald uppercase text-5xl text-red-50 hover:text-red-500 "
 				>
 					Page under construction
-				</div>
+				</Link>
 			))}
 		</div>
 	);
