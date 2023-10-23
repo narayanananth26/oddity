f1d4b6e75455678bf9cd2ec440a6bfc64210f70a
Ananth Narayan <narayanananth26@gmail.com>
2023-10-23 21:38:51 +0530

    Add session callback

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -10,13 +10,42 @@ const handler = NextAuth({
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbacks: {
+				async signIn(user, account, metadata) {
+					const { email, name, image } = metadata;
+
+					// Connect to the database
+					await connectToMongoDB();
+					console.log(email, name, image);
+
+					// Check if the user already exists in your database
+					let existingUser = await User.findOne({ email });
+
+					if (!existingUser) {
+						// If the user doesn't exist, create a new user record
+						existingUser = await User.create({
+							email,
+							username: name.replace(" ", "").toLowerCase(),
+							image,
+							// Add other user fields as needed
+						});
+					} else {
+						// If the user exists, update their data
+						existingUser.image = image;
+						// Update other user fields as needed
+						await existingUser.save();
+					}
+
+					return true;
+				},
+			},
 		}),
 		CredentialsProvider({
 			id: "credentials",
 			name: "Credentials",
 			credentials: {
-				username: { label: "Username", type: "text" }, // Add a username field
-				password: { label: "Password", type: "password" }, // Add a password field
+				username: { label: "Username", type: "text" },
+				password: { label: "Password", type: "password" },
 			},
 			async authorize(credentials) {
 				await connectToMongoDB();
@@ -41,11 +70,36 @@ const handler = NextAuth({
 						throw new Error("User not found!");
 					}
 				} catch (error) {
-					throw new Error(error);
+					console.log(error);
+					return null;
 				}
 			},
 		}),
 	],
+	callbacks: {
+		async session({ session, token }) {
+			try {
+				// Ensure that `User.findOne` query succeeds
+				const sessionUser = await User.findOne({
+					email: session.user.email,
+				});
+
+				if (sessionUser) {
+					console.log(sessionUser);
+					// Access user data if it exists
+					session.user.username = sessionUser.username;
+					session.user.image = sessionUser.image;
+				}
+
+				return session;
+			} catch (error) {
+				// Handle any errors gracefully
+				console.error("Session callback error:", error);
+				return session;
+			}
+		},
+	},
+
 	pages: {
 		error: "/sign-in",
 	},
