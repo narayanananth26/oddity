86ab468147de0e3c15c096a299daa48308228907
Ananth Narayan <narayanananth26@gmail.com>
2023-10-23 21:41:08 +0530

    Add image field

--- src/models/User.js
+++ src/models/User.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
+		image: { type: String, default: "/assets/user.svg", required: true },
 	},
 	{ timestamps: true }
 );
