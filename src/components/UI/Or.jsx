04eaa92c5f75342683350fff8f305b8901f99e16
Ananth Narayan <narayanananth26@gmail.com>
2023-10-23 21:41:01 +0530

    Update positioning of elements

--- src/components/UI/Or.jsx
+++ src/components/UI/Or.jsx
@@ -2,7 +2,7 @@ const Or = () => {
 	return (
 		<div className="flex-between relative my-3">
 			<span className="border-b-2 border-slate-300 w-full h-0" />
-			<span className="text-slate-500 absolute right-1/2 translate-x-1/2 z-50 bg-red-50 px-3">
+			<span className="flex-center text-slate-500 absolute right-1/2 translate-x-1/2 z-50 h-fit bg-red-50 px-3">
 				or
 			</span>
 		</div>
