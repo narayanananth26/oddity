24a4a9eb74469f8f65d0ea687ef3034cd29e7655
Ananth Narayan <narayanananth26@gmail.com>
2023-10-24 12:46:04 +0530

    Update NavLayout.jsx

--- src/components/Navigation/NavLayout.jsx
+++ src/components/Navigation/NavLayout.jsx
@@ -1,6 +1,8 @@
 const NavLayout = ({ layout, children }) => {
 	return (
-		<nav className={`${layout} rounded py-3 px-10 text-xl bg-red-50 z-50`}>
+		<nav
+			className={`${layout} absolute top-3 left-3 w-[98%] rounded py-2 px-10 text-xl bg-red-50 z-50 h-[70px]`}
+		>
 			{children}
 		</nav>
 	);
