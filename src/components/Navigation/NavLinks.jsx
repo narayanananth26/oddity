7b755b4faea922ed4f5767c5b017f84099f85921
Ananth Narayan <narayanananth26@gmail.com>
2023-10-24 12:46:13 +0530

    Update NavLinks.jsx

--- src/components/Navigation/NavLinks.jsx
+++ src/components/Navigation/NavLinks.jsx
@@ -1,10 +1,20 @@
-import { useSession } from "next-auth/react";
+import { useState } from "react";
+import Button from "@components/UI/Button";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { signInLink } from "@utils/constants/links";
+import { useRouter } from "next/navigation";
 
 const NavLinks = ({ links, pathname, positioning }) => {
 	const { data: session } = useSession();
-	console.log(session);
+	const router = useRouter();
+	const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsDropdownVisible(!isDropdownVisible);
+	};
+
 	return (
 		<div className={positioning}>
 			{links.map((link, index) => {
@@ -12,22 +22,46 @@ const NavLinks = ({ links, pathname, positioning }) => {
 					(pathname.includes(link.route) && link.route.length > 1) ||
 					pathname === link.route;
 				return link.route === "/sign-in" && session ? (
-					<Link
-						href="/profile"
-						className="flex-center gap-2"
-						key={index}
-					>
-						<Image
-							src={session?.user.image || "/assets/user.svg"}
-							width={37}
-							height={37}
-							className="rounded-full"
-							alt="Profile"
-						/>
-						<span className="w-fit hover:text-red-500">
-							{session?.user.username || "User"}
-						</span>
-					</Link>
+					<div key={index} className="relative">
+						<div
+							onClick={toggleDropdown}
+							className="flex-center gap-2 hover:text-red-500 cursor-pointer"
+							style={{ position: "relative" }} // Set relative positioning
+						>
+							<Image
+								src={session?.user.image}
+								width={37}
+								height={37}
+								className="rounded-full"
+								alt="Profile"
+							/>
+							<span>{session?.user.username}</span>
+						</div>
+
+						{isDropdownVisible && (
+							<div
+								className="absolute right-0 mt-2"
+								style={{ width: "100%" }} // Set the width to 100%
+							>
+								<div className="w-40 bg-white border rounded shadow">
+									<Link href="/profile">
+										<span className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+											Profile
+										</span>
+									</Link>
+									<button
+										onClick={() => {
+											signOut();
+											setIsDropdownVisible(false);
+										}}
+										className="block px-4 py-2 text-red-500 hover:bg-red-100 w-full flex-start"
+									>
+										Sign Out
+									</button>
+								</div>
+							</div>
+						)}
+					</div>
 				) : (
 					<Link
 						key={index}
@@ -40,7 +74,7 @@ const NavLinks = ({ links, pathname, positioning }) => {
 							className={`${
 								isActive &&
 								"text-red-50 hover:text-white hover:after:bg-red-50 active:text-black"
-							} flex-center relative text-2xl uppercase tracking-wider text-black hover:text-red-500 active:text-black pb-1 underline_animation after:bg-red-500 after:h-[3px] `}
+							} flex-center relative text-2xl uppercase tracking-wider text-black hover:text-red-500 active:text-black pb-1 underline_animation after:bg-red-500 after:h-[3px]`}
 						>
 							{link.label}
 						</span>
