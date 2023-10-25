01677b23122d6a09ecd464ffe681e82eeff818fa
Ananth Narayan <narayanananth26@gmail.com>
2023-10-25 17:18:54 +0530

    Update Logo styling

--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Logo = ({ width, height }) => {
+const Logo = () => {
 	return (
 		<div className="flex-center w-full">
-			<Link href="/" className="relative flex-center h-full w-full">
+			<Link href="/" className="absolute flex-center h-full">
 				<Image
 					src="/assets/oddity-logo.svg"
 					alt="Oddity logo"
 					priority
-					width={width}
-					height={height}
+					width="0"
+					height="0"
 					className="transition-transform hover:brightness-75 active:brightness-100 h-full w-auto"
 				/>
 			</Link>
