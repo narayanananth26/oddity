a3c70d467531af8df69aa66ff0e364b88c8c5e67
Ananth Narayan <narayanananth26@gmail.com>
2023-10-26 12:39:53 +0530

    Update page.jsx

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -4,16 +4,19 @@ import { apiUrl } from "@utils/constants/links";
 
 const getLiveEvent = async () => {
 	const res = await fetch(`${apiUrl}/events?filterBy=status&status=Live`, {
-		next: { revalidate: 3600 },
+		next: { revalidate: 60 },
 	});
 	if (!res) throw new Error("Failed to fetch events");
 
 	const events = await res.json();
 
-	const event = events.reduce((maxEvent, event) => {
-		if (event.bets_placed > maxEvent.bets_placed) return event;
-		return maxEvent;
-	});
+	let event = {};
+
+	if (events.length)
+		event = events.reduce((maxEvent, event) => {
+			if (event.bets_placed > maxEvent.bets_placed) return event;
+			return maxEvent;
+		});
 
 	return event;
 };
