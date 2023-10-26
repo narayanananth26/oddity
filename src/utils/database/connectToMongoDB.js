1b5694ad0b947267c547b36112bc1e5bf4dcccc0
Ananth Narayan <narayanananth26@gmail.com>
2023-10-26 12:41:17 +0530

    Update connectToMongoDB.jsx

--- src/utils/database/connectToMongoDB.js
+++ src/utils/database/connectToMongoDB.js
@@ -5,12 +5,18 @@ let isConnected = false;
 const connectToMongoDB = async () => {
 	try {
 		if (isConnected) {
-			console.log("MongoDB connection already established");
+			console.log(
+				"connectToMongoDB.js - isConnected?\n",
+				"MongoDB connection already established"
+			);
 			return;
 		}
 
 		if (!process.env.MONGODB_URL) {
-			throw new Error("Missing MongoDB URL");
+			throw new Error(
+				"connectToMongoDB.js - process.ENV.MONGODB_URL?\n",
+				"Missing MongoDB URL"
+			);
 		}
 
 		await mongoose.connect(process.env.MONGODB_URL, {
@@ -19,15 +25,22 @@ const connectToMongoDB = async () => {
 		});
 
 		isConnected = true;
-		console.log("MongoDB connected");
+		console.log("connectToMongoDB.js\n", "MongoDB connected");
 	} catch (error) {
-		console.error("MongoDB connection error:", error.message);
+		console.error(
+			"connectToMongoDB.js\n",
+			"MongoDB connection error:",
+			error.message
+		);
 	}
 };
 
 // Reconnection handling
 mongoose.connection.on("disconnected", () => {
-	console.log("MongoDB disconnected. Reconnecting...");
+	console.log(
+		"connectToMongoDB.js\n",
+		"MongoDB disconnected. Reconnecting..."
+	);
 	isConnected = false;
 	setTimeout(() => connectToMongoDB(), 5000); // Attempt reconnection after 5 seconds
 });
