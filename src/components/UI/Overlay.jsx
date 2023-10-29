29abe5961f9a77c29d038b9a3327780258672ed2
Ananth Narayan <narayanananth26@gmail.com>
2023-10-29 11:35:31 +0530

    Add Overlay.jsx

---
+++ src/components/UI/Overlay.jsx
@@ -0,0 +1,9 @@
+const Overlay = () => {
+	return (
+		<div
+			className={`absolute inset-0 bg-black transition-opacity ease-in-out duration-500 opacity-70`}
+		/>
+	);
+};
+
+export default Overlay;
