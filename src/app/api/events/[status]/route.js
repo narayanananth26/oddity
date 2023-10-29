8f7ff08c216f6f564e062df8a1eb0f8f062e7627
Ananth Narayan <narayanananth26@gmail.com>
2023-10-29 11:34:17 +0530

    Update route.js

---
+++ src/app/api/events/[status]/route.js
@@ -0,0 +1,20 @@
+import Event from "@models/Event";
+import { connectToMongoDB } from "@utils/database/connectToMongoDB";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+	try {
+		await connectToMongoDB();
+		const { status } = params;
+		const filteredEvents = await Event.find({ status });
+
+		return new NextResponse(JSON.stringify(filteredEvents), {
+			status: 200,
+		});
+	} catch (error) {
+		console.log("events/route.js\n", error);
+		return new NextResponse(error.message, {
+			status: 500,
+		});
+	}
+};
