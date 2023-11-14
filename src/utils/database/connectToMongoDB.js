b343dd970b31f603b8fe9a0e18177b83eb8c460a
Ananth Narayan <narayanananth26@gmail.com>
2023-11-14 09:24:52 +0530

    Update console log in connectToMongoDB.js

--- src/utils/database/connectToMongoDB.js
+++ src/utils/database/connectToMongoDB.js
@@ -5,18 +5,12 @@ let isConnected = false;
 const connectToMongoDB = async () => {
 	try {
 		if (isConnected) {
-			console.log(
-				"connectToMongoDB.js - isConnected?\n",
-				"MongoDB connection already established"
-			);
+			console.log("MongoDB connection already established");
 			return;
 		}
 
 		if (!process.env.MONGODB_URL) {
-			throw new Error(
-				"connectToMongoDB.js - process.ENV.MONGODB_URL?\n",
-				"Missing MongoDB URL"
-			);
+			throw new Error("Missing MongoDB URL");
 		}
 
 		await mongoose.connect(process.env.MONGODB_URL, {
