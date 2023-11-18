3218e12cbe38f3833a3dfa23fc2c4a7461b4073b
Ananth Narayan <narayanananth26@gmail.com>
2023-11-18 22:19:22 +0530

    Remove getAllNews.js

--- src/utils/fetch-functions/getAllNews.js
+++
@@ -1,9 +0,0 @@
-import { apiUrl } from "@utils/constants/links";
-
-export const getAllNews = async () => {
-	const res = await fetch(`${apiUrl}/news`, {
-		next: { revalidate: 60 },
-	});
-	if (!res) throw new Error("Failed to fetch news");
-	return await res.json();
-};
