050cc5ba0454331885eb883feb800935c0d65f53
Ananth Narayan <narayanananth26@gmail.com>
2023-11-20 20:35:05 +0530

    Create EventVenue.jsx

---
+++ src/components/UI/EventVenue.jsx
@@ -0,0 +1,12 @@
+import { HiMapPin } from "react-icons/hi2";
+
+const EventVenue = ({ venue, className }) => {
+	return (
+		<span className={`flex-center gap-2 ${className}`}>
+			<HiMapPin />
+			{venue?.name || ""}
+		</span>
+	);
+};
+
+export default EventVenue;
