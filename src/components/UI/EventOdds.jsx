a399db104a8ff65d730ceb1e62c8a71ad3109b59
Ananth Narayan <narayanananth26@gmail.com>
2023-11-20 20:34:54 +0530

    Create EventOdds.jsx

---
+++ src/components/UI/EventOdds.jsx
@@ -0,0 +1,29 @@
+const Odds = ({ odds, className, oddClassName }) => {
+	return (
+		<div className={className}>
+			<span
+				className={`${
+					odds?.home_team > odds?.away_team
+						? "before:content-['-'] text-red-500"
+						: "before:content-['+'] text-green-500"
+				} ${oddClassName}`}
+			>
+				{odds?.home_team}
+			</span>
+			<span className={`before:content-['+'] ${oddClassName}`}>
+				{odds?.draw}
+			</span>
+			<span
+				className={`${
+					odds?.away_team > odds?.home_team
+						? "before:content-['-'] text-red-500"
+						: "before:content-['+'] text-green-500"
+				} ${oddClassName}`}
+			>
+				{odds?.away_team}
+			</span>
+		</div>
+	);
+};
+
+export default Odds;
