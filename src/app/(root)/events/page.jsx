9a5000f6da1ece840e34cb00f877e9be547be0a7
Ananth Narayan <narayanananth26@gmail.com>
2023-11-21 20:12:10 +0530

    Update events/page.jsx

--- src/app/(root)/events/page.jsx
+++ src/app/(root)/events/page.jsx
@@ -6,8 +6,13 @@ const page = () => {
 	return (
 		<>
 			<EventsBanner />
+			<div className="flex-center relative mt-24 mb-16 text-5xl uppercase text-red-500 bg-white w-full">
+				<span className="flex-center border-b-2 border-slate-300 w-3/4 h-0" />
+				<span className="flex-center absolute right-1/2 translate-x-1/2 z-10 h-fit bg-inherit px-3">
+					Events
+				</span>
+			</div>
 			<EventsList />
-			<UnderConstruction />
 		</>
 	);
 };
