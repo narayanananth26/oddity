39b461febb6da39b135f6b07275238ce912a0560
Ananth Narayan <narayanananth26@gmail.com>
2023-11-22 22:46:59 +0530

    Create NavLayout.jsx

--- src/components/Navigation/NavLayout.jsx
+++ src/components/Navigation/NavLayout.jsx
@@ -21,9 +21,7 @@ const NavLayout = ({ layout, children }) => {
 
 	return (
 		<nav
-			className={`${layout} absolute top-3 left-3 w-[98%] backdrop-blur-xl rounded-xl py-2 px-10 text-xl ${
-				scrolling ? "bg-red-50 text-black" : "text-red-500"
-			} z-50 h-[70px]`}
+			className={`${layout} absolute top-3 left-3 w-[98%] backdrop-blur-xl rounded-xl py-2 px-10 text-xl border-spacing-8 text-red-500 z-50 h-[70px]`}
 		>
 			{children}
 		</nav>
