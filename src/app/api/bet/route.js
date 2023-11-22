a7b8f2efdb70c35f8ae43d9da3b71f814c3cd39d
Ananth Narayan <narayanananth26@gmail.com>
2023-11-22 22:44:46 +0530

    Create bet/route.js

---
+++ src/app/api/bet/route.js
@@ -0,0 +1,34 @@
+import Bet from "@models/Bet";
+import { connectToMongoDB } from "@utils/database/connectToMongoDB";
+import { NextResponse } from "next/server";
+
+export const POST = async (req, res) => {
+	try {
+		const { user, event, stakeAmount, payout } = await req.json();
+
+		await connectToMongoDB();
+
+		const newBet = {
+			user,
+			event,
+			stake_amount: stakeAmount,
+			payout,
+		};
+
+		console.log(newBet);
+
+		const bet = await Bet.create(newBet);
+
+		return new NextResponse(JSON.stringify(bet), {
+			status: 201,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	} catch (error) {
+		console.error("Error placing bet:", error);
+		return new NextResponse(error.message, {
+			status: 500,
+		});
+	}
+};
