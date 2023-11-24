99c0f2f9f99ee1a56835561ed7cf499974ecc302
Ananth Narayan <narayanananth26@gmail.com>
2023-11-24 16:46:32 +0530

    Create admin/page.jsx

---
+++ src/app/(root)/admin/page.jsx
@@ -0,0 +1,11 @@
+import AdminContent from "@components/Admin/AdminContent";
+
+const admin = () => {
+	return (
+		<div className="w-screen h-screen">
+			<AdminContent />
+		</div>
+	);
+};
+
+export default admin;
